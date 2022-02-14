1a1822db95e20a362e59eaa8dba307df766f1bee
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2022-02-14 21:15:15 +0300

    git init

---
+++ src/test/java/com/barrista/jdm/JdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrista.jdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
