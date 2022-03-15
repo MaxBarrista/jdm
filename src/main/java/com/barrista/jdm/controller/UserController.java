b5284d2569984e2367fdbcb0c9710d32fdbe89f4
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2022-03-15 14:40:36 +0300

    feat: add access rights for roles

--- src/main/java/com/barrista/jdm/controller/UserController.java
+++ src/main/java/com/barrista/jdm/controller/UserController.java
@@ -3,6 +3,7 @@ package com.barrista.jdm.controller;
 import com.barrista.jdm.domain.Role;
 import com.barrista.jdm.domain.User;
 import com.barrista.jdm.repos.UserRepo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
+@PreAuthorize("hasAuthority('ADMIN')")
 @RequestMapping("/user")
 public class UserController
 {
